7713abc76d342bad484393c0edb1770fe1fe59b9
Sebastian Ramacher <sebastian@ramacher.at>
2021-02-03 00:25:22 +0100

    Move meta data to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 
 import platform
-import os.path
 from setuptools import setup, Extension
-
-
-def read(name):
-    with open(os.path.join(os.path.dirname(__file__), name), encoding="utf-8") as f:
-        return f.read()
 
 
 extension_sources = ["cwcwidth/_impl.pyx"]
@@ -30,28 +24,9 @@ ext_modules = [
 ]
 
 setup(
-    name="cwcwidth",
-    version="0.1.2",
-    description="Python bindings for wc(s)width",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    author="Sebastian Ramacher",
-    author_email="sebastian@ramacher.at",
-    url="https://github.com/sebastinas/cwcwidth",
-    license="Expat",
     ext_modules=ext_modules,
     packages=["cwcwidth"],
     package_data={
-        "cwcwidth": ["*.pyi", "py.typed"],
+        "cwcwidth": ["_impl.pyi", "py.typed"],
     },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    python_requires=">= 3.6",
-    setup_requires=setup_requires,
-    zip_safe=False,
 )
