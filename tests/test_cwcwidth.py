163c4b0f18fa843115686def3cfae036090f3491
Sebastian Ramacher <sebastian@ramacher.at>
2021-02-07 17:56:08 +0100

    Fix typo

--- tests/test_cwcwidth.py
+++ tests/test_cwcwidth.py
@@ -1,3 +1,23 @@
+# Copyright 2021 Sebastian Ramacher <sebastian@ramacher.at>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import locale
 import unittest
 from cwcwidth import wcwidth, wcswidth
@@ -25,7 +45,7 @@ class Tests(unittest.TestCase):
             wcwidth("abc")
 
     def test_empty_string(self):
-        """Width of an emptry string is 0."""
+        """Width of an empty string is 0."""
         self._exec_test("", tuple())
 
     def test_hello_world(self):
